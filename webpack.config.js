e2c52d1f80c957a261ea0a0ea54a3f79406f2e8f
Jeff Dye <jeffdye@Jeffs-MBP.fios-router.home>
2018-12-23 17:58:03 -0500

    Better import/export/global

--- webpack.config.js
+++ webpack.config.js
@@ -16,7 +16,8 @@ module.exports = {
   },
   output: {
     filename: 'bundle.js',
-    path: path.join(__dirname, 'public')
+    path: path.join(__dirname, 'public'),
+    libraryTarget: 'window'
   },
   devServer: {
     contentBase: path.join(__dirname, 'public'),
