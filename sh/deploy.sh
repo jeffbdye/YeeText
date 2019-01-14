3e9f05b354bd9454a773ea1485d36a5c1e6c9fde
Jeff Dye <jeffdye@Jeffs-MacBook-Pro.local>
2019-01-13 20:11:55 -0500

    Just a little more setup required

--- sh/deploy.sh
+++ sh/deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 yeetext_now=`date +%Y-%m-%d--%H-%M-%S`
 mkdir /var/www/yeetext.net/archive/$yeetext_now
-mv /var/www/yeetext.net/public /var/www/yeetext.net/$yeetext_now
+mv /var/www/yeetext.net/public /var/www/yeetext.net/archive/$yeetext_now
 cp /var/lib/jenkins/workspace/YeeText/public/* /var/www/yeetext.net/public/
 unset yeetext_now
