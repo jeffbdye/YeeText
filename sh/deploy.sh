4faadd36505621b1292be99faa9fa7f3a9d2b7ff
Jeff Dye <jeffdye@Jeffs-MacBook-Pro.local>
2019-01-13 20:28:16 -0500

    Try this path

--- sh/deploy.sh
+++ sh/deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 yeetext_now=`date +%Y-%m-%d--%H-%M-%S`
 mkdir /var/www/yeetext.net/archive/$yeetext_now
-mv /var/www/yeetext.net/public/* /var/www/yeetext.net/archive/$yeetext_now/public
+mv /var/www/yeetext.net/public/* /var/www/yeetext.net/archive/$yeetext_now
 mkdir /var/www/yeetext.net/public
 cp /var/lib/jenkins/workspace/YeeText/public/* /var/www/yeetext.net/public
 chgrp buildusers /var/www/yeetext.net/public
