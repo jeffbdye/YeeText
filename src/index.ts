980592ef81299a89d2a164e52334edf49eb3e11e
Jeff Dye <jeffdye@Jeffs-MacBook-Pro.local>
2019-01-20 21:45:43 -0500

    Add title casing

--- src/index.ts
+++ src/index.ts
@@ -1,34 +1,56 @@
 import { clap, alternate } from './yee';
 import copy from 'copy-to-clipboard';
 
+const inputElement = document.getElementById('input-text') as HTMLTextAreaElement;
+const resultElement = document.getElementById('yeetext') as HTMLTextAreaElement;
+const yeetElement = document.getElementById('yeet') as HTMLInputElement;
+const alertnatingElement = document.getElementById('aLtErNaTiNg') as HTMLInputElement;
+const jalenElement = document.getElementById('Jalen-Smith') as HTMLInputElement;
+const copyButton = document.getElementById('copy') as HTMLButtonElement;
+
 function yeet(value: string) {
-  document.getElementById('yeetext').innerHTML = yeetThatText(value);
+  resultElement.innerHTML = yeetThatText(value);
 }
 
-function optionToggled() {
-  const yeetElement = document.getElementById('input-text') as HTMLTextAreaElement;
-  yeet(yeetElement.value);
+function optionToggled(e: Event) {
+  validateForm(e);
+  applyStyles();
+  yeet(inputElement.value);
 }
 
 function yeetThatText(text: string) {
-  const yeetSelected = getCheckboxValue('yeet');
-  const alternateSelected = getCheckboxValue('aLtErNaTiNg');
+  const yeetSelected = yeetElement.checked;
+  const alternateSelected = alertnatingElement.checked;
+
   if (yeetSelected && alternateSelected) {
     return alternate(clap(text));
   } else if (yeetSelected) {
     return clap(text);
-  } else {
+  } else if (alternateSelected) {
     return alternate(text);
+  } else {
+    return text;
   }
 }
 
-function copyClicked() {
-  copy(document.getElementById('yeetext').textContent);
-  document.getElementById('copy').innerText = 'Copied!';
+function validateForm(e: Event) {
+  // enforce eclusive capitalization options, since these
+  // are like radio buttons with an implied 'neither selected' option
+  if (e.srcElement.id === 'aLtErNaTiNg') {
+    jalenElement.checked = false;
+  } else if (e.srcElement.id === 'Jalen-Smith') {
+    alertnatingElement.checked = false;
+  }
 }
 
-function getCheckboxValue(elementId: string): boolean {
-  return (document.getElementById(elementId) as HTMLInputElement).checked;
+function applyStyles() {
+  const jalenSelected = jalenElement.checked;
+  resultElement.style.textTransform = jalenSelected ? 'capitalize' : '';
+}
+
+function copyClicked() {
+  copy(resultElement.textContent);
+  copyButton.innerText = 'Copied!';
 }
 
 export { yeet, copyClicked, optionToggled };
