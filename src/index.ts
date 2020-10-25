259b976317ef324643891a79d0e9c87ed9517d32
Jeff Dye <jeffbdye@gmail.com>
2020-10-25 19:42:02 -0400

    Fix errors with falsy inputs (#19)
    * npm audit fixes
    
    * prevent falsy input error
    
    * use target in place of srcElement

--- src/index.ts
+++ src/index.ts
@@ -8,16 +8,20 @@ const spongebobElement = document.getElementById('sPoNgEbOb') as HTMLInputElemen
 const jadenElement = document.getElementById('Jaden-Smith') as HTMLInputElement;
 const copyButton = document.getElementById('copy') as HTMLButtonElement;
 
-function yeet(value: string) {
+function yeetOnInput(value: string) {
   resultElement.innerHTML = yeetThatText(value);
 }
 
 function optionToggled(e: Event) {
   validateForm(e);
-  yeet(inputElement.value);
+  yeetOnInput(inputElement.value);
 }
 
-function yeetThatText(text: string) {
+function yeetThatText(text: string): string {
+  if (!text) {
+    return text;
+  }
+
   const yeetSelected = yeetElement.checked;
   const spongebobSelected = spongebobElement.checked;
   const jadenSelected = jadenElement.checked;
@@ -41,7 +45,7 @@ function yeetThatText(text: string) {
 function validateForm(e: Event) {
   // enforce eclusive capitalization options, since these
   // are like radio buttons with an implied 'neither selected' option
-  const srcElement = e.srcElement as HTMLElement;
+  const srcElement = e.target as HTMLElement;
   if (srcElement.id === 'sPoNgEbOb') {
     jadenElement.checked = false;
   } else if (srcElement.id === 'Jaden-Smith') {
@@ -54,4 +58,4 @@ function copyClicked() {
   copyButton.innerText = 'Copied!';
 }
 
-export { yeet, copyClicked, optionToggled };
+export { yeetOnInput, copyClicked, optionToggled };
